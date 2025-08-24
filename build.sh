9a8063c0092f5142de9ce618ca1eed3e584e65df
wess-westley <westleykanyora9@gmail.com>
2025-08-24 20:48:24 +0300

    Fix: remove submodule metadata, keep core as normal folder

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 set -o errexit  
 
 pip install -r requirements.txt
+
+# Move into core where manage.py is
+cd core
+
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-# Start Gunicorn (Django entry point)
-gunicorn backend.core.wsgi:application --bind 0.0.0.0:8000 --workers 3
