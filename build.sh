9b30baaa755bdaa79b472c7d5f83038fb9ee39d6
wess-westley <westleykanyora9@gmail.com>
2025-08-24 22:59:11 +0300

    some changes to the placement of folders in order to deploy it

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
+
 echo "Current directory: $(pwd)"
 ls -la
-ls -la core/
+ls -la backend/core/
+
 pip install -r requirements.txt
 
-
+export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 
 gunicorn backend.core.wsgi:application --bind 0.0.0.0:8000 --workers 3
-
