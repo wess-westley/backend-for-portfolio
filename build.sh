0a18e20efca76188fb110b3e9ee52c185227bce0
wess-westley <westleykanyora9@gmail.com>
2025-08-24 20:05:39 +0300

    Fix build script and gunicorn command

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
-set -o errexit
+#!/usr/bin/env bash
+set -o errexit  
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py makemigrations
-gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
+
+# Start Gunicorn (Django entry point)
+gunicorn backend.core.wsgi:application --bind 0.0.0.0:8000 --workers 3
