52d1cd1138b08fcc0009dfbf85effd6f266087f0
wess-westley <westleykanyora9@gmail.com>
2025-08-24 22:43:53 +0300

    some changes to the placement of folders in order to deploy it

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
+echo "Current directory: $(pwd)"
+ls -la
+ls -la core/
+pip install -r requirements.txt
 
-pip install -r backend/requirements.txt
 
-python backend/core/manage.py collectstatic --no-input
-python backend/core/manage.py migrate
 
-gunicorn backend/core.wsgi:application --bind 0.0.0.0:8000 --workers 3
+gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 3
