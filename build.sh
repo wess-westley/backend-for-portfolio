963b967f6e265def2ac33b62bbb0f17759d35c5d
wess-westley <westleykanyora9@gmail.com>
2025-08-24 21:31:20 +0300

    some changes done procfilr

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ pip install -r requirements.txt
 # Always run commands from repo root
 cd "$(dirname "$0")"
 
-python core/manage.py collectstatic --no-input
-python core/manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
 
 # Start Gunicorn (Django entry point)
 gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 3
