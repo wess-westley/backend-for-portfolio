cc5591b1248be932b7ae20f670875f3567b39b77
wess-westley <westleykanyora9@gmail.com>
2025-08-24 23:39:06 +0300

    some changes to the placement of folders in order to deploy it

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "Current directory: $(pwd)"
-ls -la
-ls -la core/
-
 pip install -r requirements.txt
 
-gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 3
+gunicorn core.cause.wsgi:application --bind 0.0.0.0:8000 --workers 3
