7634e218a026cafe560a9f03c09484b9f00cdef4
wess-westley <westleykanyora9@gmail.com>
2025-08-24 22:12:44 +0300

    some changes to the placement of folders

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -o errexit  
+
+
+pip install -r requirements.txt
+
+python core/manage.py collectstatic --no-input
+python core/manage.py migrate
+
+gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 3
