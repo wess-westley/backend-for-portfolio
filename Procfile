12ec15f3daaeeb780cd6dcce79cf498ed4ad0223
wess-westley <westleykanyora9@gmail.com>
2025-08-24 23:08:21 +0300

    some changes to the placement of folders in order to deploy it

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn .core.wsgi:application --bind 0.0.0.0:$PORT
+web: gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
--- build.sh
+++ build.sh
@@ -3,10 +3,8 @@ set -o errexit
 
 echo "Current directory: $(pwd)"
 ls -la
-ls -la backend/core/
+ls -la core/
 
 pip install -r requirements.txt
-
-export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 
 gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 3
